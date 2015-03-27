c09e25b0f0f6f913bf8e67c79c1ec52e61c6366b
Robert Mulholand <robmulholand@gmail.com>
2015-03-27 16:54:55 -0500

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get 'account_activations/edit'
-
   root                  "static_pages#home"
 
   get     "help"    =>  "static_pages#help"
@@ -15,8 +9,13 @@ Rails.application.routes.draw do
   post    "login"   =>  "sessions#create"
   delete  "logout"  =>  "sessions#destroy"
 
-  resources :users
+  resources :users do
+    member do 
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
